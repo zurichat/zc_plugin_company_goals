2a66a8b4dab553fe189f1600fd13052af74e3e46
Boro Omokugbo <omokugbobr@gmail.com>
2021-08-30 22:48:50 +0100

    feat(./): Update server.js file.

---
+++ server.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/order */
+/* eslint-disable import/first */
+require('dotenv').config({ path: './config.env' });
+
+const logger = require('./utils/logger');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 4000;
+
+const server = app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(`Unhandled rejection at ${promise}, reason: ${reason.message}`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM Received, Shutting down gracefully');
+
+  server.close(() => {
+    logger.info('Process Terminated because of SIGTERM');
+  });
+});
