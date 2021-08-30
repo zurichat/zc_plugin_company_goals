99f48a9fef7eb94da00bcee296b22921d16975ba
Boro Omokugbo <omokugbobr@gmail.com>
2021-08-30 22:42:52 +0100

    fix: Remove unwanted files from git cache

--- controllers/goalController.js
+++
@@ -1,9 +0,0 @@
-const catchAsync = require('../utils/catchAsync');
-
-exports.getAllGoals = catchAsync(async (req, res, next) => {
-  // If this promise is rejected, catchAsync would catch it
-  // and send it to the globalErrorHandler
-  await Promise.resolve('hi');
-
-  res.status(200).json({ status: 'success', data: [{ foo: 'bar' }] });
-});
--- routes/goalRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-
-const goalController = require('../controllers/goalController');
-
-const router = Router();
-
-router.route('/').get(goalController.getAllGoals);
-
-module.exports = router;
