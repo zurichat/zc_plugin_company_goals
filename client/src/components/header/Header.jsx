4e1daae54ddcce9d625ab2a844b813eef44540be
Isah Ibrahim <isahibn08@gmail.com>
2021-08-31 17:04:22 +0100

    added a header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,18 +1,35 @@
-import { StyledAppBar, StyledToolBar } from './Header.styled';
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import TextField from '@material-ui/core/TextField'
+import AddIcon from '@material-ui/icons/Add';
 
-const AppHeader = () => {
-  return (
-    <StyledAppBar position="static">
-      <StyledToolBar>
-        <h1>
-          Welcome to Zuri Chat Goals Plugin{' '}
-          <span role="img" aria-label="celebrate emoji">
-            🥳
-          </span>
-        </h1>
-      </StyledToolBar>
-    </StyledAppBar>
-  );
-};
+const Header = () => {
+    return (
+        <>
+            <Container>
+                <Box container display="flex" justifyContent="space-evenly" style={{marginTop:50}}>
+                    <Box>
+                        <Box container display="flex" style={{ width:500 , justifyContent:'space-between', alignItems:'center',borderBottom:'3px solid #FFF0F0'}}>
+                            <Button style={{fontWeight:'bold'}}>Mission</Button>
+                            <Button startIcon={<AddIcon />} style={{fontWeight:'bold'}} >Edit Mission</Button>
+                        </Box>
+                        <TextField placeholder="Training A Million Youths Yearly" variant="outlined" style={{width:500 , border:"none", backgroundColor:'white', outline:'none'}}/>
+                    </Box>
+                    
+                    <Box>
+                        <Box container display="flex" style={{fontWeight:'bold', width:500, justifyContent:'space-between' , alignItems:'center',borderBottom:'3px solid#FFF0F0'}}>
+                            <Button style={{fontWeight:'bold'}}>Vision</Button>
+                            <Button startIcon={<AddIcon />} style={{fontWeight:'bold'}}>Edit Mission</Button>
+                        </Box>
+                        <TextField placeholder="Tech Hub Builder" variant="outlined" style={{width:500 , border:"none", backgroundColor:'white', outline:'none'}}/>
+                    </Box>
+                    
+                </Box>
+            </Container>
+        </>
+    )
+}
 
-export default AppHeader;
+export default Header
