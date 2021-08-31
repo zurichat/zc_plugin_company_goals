0195413ab5c7c60e855b8ac9d7873cc922d1df5b
adepeju4 <adepeju.kayode@gmail.com>
2021-08-31 21:50:32 +0100

    fix: remove async

--- controllers/sidebarController.js
+++ controllers/sidebarController.js
@@ -1,10 +1,7 @@
-/* eslint-disable no-console */
 const sidebarOptions = require('../data/sidebarData.json');
 
-const catchAsync = require('../utils/catchAsync');
-
-const readSidebar = catchAsync(async (req, res) => {
+const readSidebar = (req, res) => {
   return res.status(200).json(sidebarOptions);
-});
+};
 
 module.exports = readSidebar;
