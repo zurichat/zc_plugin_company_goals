214b1313d4a5c9252b206d2ab553f096112ca70c
Isah Ibrahim <isahibn08@gmail.com>
2021-09-01 20:53:22 +0100

    added the last button

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import Container from '@material-ui/core/Container'
-import Button from '@material-ui/core/Button'
-import Box from '@material-ui/core/Box'
-import TextField from '@material-ui/core/TextField'
+import {Container , Button , Box,  TextField, ButtonGroup, Grid, Paper} from '@material-ui/core'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import AddIcon from '@material-ui/icons/Add';
 
 const Header = () => {
@@ -21,7 +19,13 @@ const Header = () => {
                     <Box>
                         <Box container display="flex" style={{fontWeight:'bold', width:500, justifyContent:'space-between' , alignItems:'center',borderBottom:'3px solid#FFF0F0'}}>
                             <Button style={{fontWeight:'bold'}}>Vision</Button>
-                            <Button startIcon={<AddIcon />} style={{fontWeight:'bold'}}>Edit Mission</Button>
+                            <div>
+                                <Button startIcon={<AddIcon />} style={{fontWeight:'bold'}}>Edit Mission</Button>
+                                <span>
+                                    <Button style={{backgroundColor:'#00B87C' , color:'white'}}><ExpandLessIcon /></Button>
+                                </span>
+                            </div>
+                            
                         </Box>
                         <TextField placeholder="Tech Hub Builder" variant="outlined" style={{width:500 , border:"none", backgroundColor:'white', outline:'none'}}/>
                     </Box>
