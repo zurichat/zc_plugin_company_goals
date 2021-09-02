5f9d1bd528222d882ac9184db63a4c0878d7557e
abiona01 <quadratwemimo@gmail.com>
2021-09-02 16:56:20 +0100

    Create goal modal created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import SimpleModal from './components/createGoal/CreateGoal';
 import AppHeader from './components/header/Header';
 
 function App() {
   return (
     <div>
       <AppHeader />
+      <SimpleModal />
     </div>
   );
 }
