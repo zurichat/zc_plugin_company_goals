f8b77fe00f6f705a0438e6655453ff70bf298ab6
Franklin <franklinokolie86@gmail.com>
2021-09-02 17:43:40 +0100

    Removed the component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import AppHeader from './components/header/Header';
-import EditVision from './components/Modal/EditVision';
 
 function App() {
   return (
     <div>
       <AppHeader />
-      <EditVision />
     </div>
   );
 }
