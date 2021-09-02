2bc23842eaaa7e1d5ae074fafa16df37c47e6341
Buks <abolarinbukolastella@gmail.com>
2021-09-02 17:44:40 +0100

    created navbar for goal interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import NavLayout from './components/goal interface navbar/NavLayout';
 import AppHeader from './components/header/Header';
 
 function App() {
   return (
     <div>
       <AppHeader />
+      <NavLayout />
     </div>
   );
 }
---
+++ client/src/components/goal interface navbar/NavName.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const NavName = styled.div`
+  height: 28px;
+  color: #999999;
+  font-family: Lato;
+  font-weight: 400;
+  text-transform: capitalize;
+  line-height: 28px;
+  padding: 0rem 1rem;
+  padding-bottom: 0.5rem;
+
+  &.active {
+    color: #00b87c;
+    font-weight: 700;
+    border-bottom: 1.5px solid #00b87c;
+  }
+`;
+
+export default NavName;
