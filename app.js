a4be5ca350fbc26c1f0c75a66811248002e50896
adepeju4 <adepeju.kayode@gmail.com>
2021-09-02 10:15:03 +0100

    fix(BE): removed /api/v1 and added Bi-annual goals

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(compression());
 // Api routes
 app.use('/api/v1/goals', rateLimiter(), goalRouter);
 app.use('/api/v1/info', rateLimiter(), pluginInfoRouter);
-app.use('/api/v1/sidebar', rateLimiter(), sidebarRouter);
+app.use('/sidebar', rateLimiter(), sidebarRouter);
 
 // To serve frontend static files in production
 if (process.env.NODE_ENV === 'production') {
