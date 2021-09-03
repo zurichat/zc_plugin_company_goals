a7631d12f8883a132bf4d803f23e483e9bfb1a34
Muwaffaq <itzmuwaffaq@gmail.com>
2021-09-03 15:09:31 +0100

    refactored images to use map function when displaying on modal

--- client/src/components/Deletemodal/Deletemodal.js
+++ client/src/components/Deletemodal/Deletemodal.js
@@ -8,6 +8,12 @@ const Deletemodal = () => {
    const exitModal = () => {
       document.querySelector('.faded-md').style.display = 'none'
    }
+   const imgData = [
+      {image: 'deleteImg', className: 'd-img1'},{image: 'deleteimg2', className: 'd-img2'},
+      {image: 'deleteimg3', className: 'd-img3'},{image: 'deleteimg3', className: 'd-img3-2'},
+      {image: 'deleteimg4', className: 'd-img4'},{image: 'deleteimg5', className: 'd-img5'},
+      {image: 'deleteimg6', className: 'd-img6'},{image: 'starimg', className: 'star-img'}
+   ]
    return (
       <>
       <button className="main-del-btn" onClick={displayModal}>Delete Goal</button>
@@ -15,14 +21,7 @@ const Deletemodal = () => {
          <div className="delete-modal">
             <button className="cancel-modal" onClick={exitModal}>X</button>
             <div className="image">
-               <img src={deleteDataImg.deleteImg} className="d-img1" alt="view" />
-               <img src={deleteDataImg.deleteimg2} className="d-img2" alt="view" />
-               <img src={deleteDataImg.deleteimg3} className="d-img3" alt="view" />
-               <img src={deleteDataImg.deleteimg3} className="d-img3-2" alt="view" />
-               <img src={deleteDataImg.deleteimg4} className="d-img4" alt="view" />
-               <img src={deleteDataImg.deleteimg5} className="d-img5" alt="view" />
-               <img src={deleteDataImg.deleteimg6} className="d-img6" alt="view" />
-               <img src={deleteDataImg.starimg} className="star-img" alt="view" />
+            {imgData.map((img) => <img src={deleteDataImg[img.image]} className={img.className} alt="view" />)}
             </div>
             <div className="delete-content">
             <h2 className="delete-head">Delete Goal ? </h2>
