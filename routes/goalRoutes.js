bafb091d29ef9927fc37e87b6e9b4350dce4febb
Olukeye <seunolukeye2000@gmail.com>
2021-09-03 22:23:29 +0100

    I edited the route

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -6,6 +6,6 @@ const router = Router();
 
 router.route('/').get(goalController.getAllGoals);
 
-router.route('/single/:id').get(goalController.getSingleGoal);
+router.route('/:id').get(goalController.getSingleGoal);
 
 module.exports = router;
