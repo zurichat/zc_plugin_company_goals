d783f333c5b3c00018c547042a35c1a5d161353c
tunde <61058516+tundealabi@users.noreply.github.com>
2021-09-03 09:35:12 +0100

    Setup/redux toolkit (#397)
    * setup base redux-toolkit configuration for the client
    
    * remove eslintcache from github
    
    * ignore eslintcache

--- client/src/components/Deletemodal/deleteAssets.js
+++ client/src/components/Deletemodal/deleteAssets.js
@@ -1,20 +1,19 @@
-import deleteImg from './deleteassets/deleteImg.png'
-import deleteimg2 from './deleteassets/deleteimg2.png'
-import deleteimg3 from './deleteassets/deleteimg3.png'
-import deleteimg4 from './deleteassets/deleteimg4.png'
-import deleteimg5 from './deleteassets/deleteimg5.png'
-import deleteimg6 from './deleteassets/deleteimg6.png'
-import starimg from './deleteassets/starimg.png'
-
+import deleteImg from './deleteassets/deleteImg.png';
+import deleteimg2 from './deleteassets/deleteimg2.png';
+import deleteimg3 from './deleteassets/deleteimg3.png';
+import deleteimg4 from './deleteassets/deleteimg4.png';
+import deleteimg5 from './deleteassets/deleteimg5.png';
+import deleteimg6 from './deleteassets/deleteimg6.png';
+import starimg from './deleteassets/starimg.png';
 
 const deleteDataImg = {
-   deleteImg,
-   deleteimg2,
-   deleteimg3,
-   deleteimg4,
-   deleteimg5,
-   deleteimg6,
-   starimg
-}
+  deleteImg,
+  deleteimg2,
+  deleteimg3,
+  deleteimg4,
+  deleteimg5,
+  deleteimg6,
+  starimg,
+};
 
-export default deleteDataImg
+export default deleteDataImg;
