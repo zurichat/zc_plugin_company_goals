cd897fdae0d48d9e45b54f4bdda532c7afd33d1a
Teniola Fatunmbi <fatunmbiteniola001@gmail.com>
2021-09-03 16:55:32 +0100

    feat: created updateGoalByID controller

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllGoals = catchAsync(async (req, res, next) => {
@@ -5,5 +7,24 @@ exports.getAllGoals = catchAsync(async (req, res, next) => {
   // and send it to the globalErrorHandler
   await Promise.resolve('hi');
 
-  res.status(200).json({ status: 'success', data: [{ foo: 'bar' }] });
+  res.status(200).json({ status: 'success', data: [{ foo: 'hy' }] });
+});
+
+exports.updateGoalByID = catchAsync(async (req, res, next) => {
+  // Get updated info from req.body
+  // const { update, $set, name } = { ...req.body } ;
+  // const data = { update: {
+  //   $set: {
+  //     name
+  //   }
+  // }}
+  const goalId = req.params.id;
+  const collectionName = 'goals';
+
+  // send the updated goal info to zuri core
+  const url = `https://test-zuri-core.herokuapp.com/crud/${collectionName}/update-by-id/${goalId}`;
+  const updatedGoal = await axios.patch(url, { ...req.body });
+
+  // send the updated goal to client.
+  return res.status(200).json(updatedGoal.data);
 });
