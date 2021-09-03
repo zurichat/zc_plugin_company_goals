51029273488f6657175157d7c4a272f0280c2b0d
Ariiie <oluwafunmilayodada2018@gmail.com>
2021-09-03 15:08:37 +0100

    adding menu-option files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
-import Header from '../components/header/Header'
+
+import Header from '../components/header/Header';
 import Mainside from '../components/mainside/Mainside';
+import Menuoption from '../components/Menuoption/Menuoption';
 import Nav from '../components/navbar/Navbar';
 
 function App() {
   return (
     <div>
-      
       <PluginSide>
         <Nav />
         <Header />
       </PluginSide>
       <Mainside />
+      <Menuoption />
     </div>
   );
 }
@@ -21,5 +23,5 @@ export default App;
 const PluginSide = styled.div`
   width: 100%;
   background-color: #f6f6f6;
-  padding-bottom:20px;
+  padding-bottom: 20px;
 `;
