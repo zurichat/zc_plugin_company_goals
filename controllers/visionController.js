3a4d78052dcda76d23a73db9b3304d70dff22105
Oluwasanmi <42076983+Dukesanmi@users.noreply.github.com>
2021-09-04 09:51:41 +0100

    Update visionController.js

--- controllers/visionController.js
+++ controllers/visionController.js
@@ -13,10 +13,12 @@ exports.addVision = (req, res)=> {
   // get the vision from client via req.body
   const { vision } = req.body;
   // insert the vision into the database
+  res.send('This is a dummy response');
 } 
 
 exports.updateVision = (req, res)=> {
   // get the new vision from client via req.body
   const { vision } = req.body;
-  // find and update the vision in the database with the edited vision statement   
+  // find and update the vision in the database with the edited vision statement
+  res.send('This is a dummy response');   
 } 
