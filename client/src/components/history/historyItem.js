65754bcae2685f36bba8cd490a62f7f48f806e8b
Ojo Triumph <ojotriumph4@gmail.com>
2021-09-04 13:02:25 +0100

    Update historyItem.js

--- client/src/components/history/historyItem.js
+++ client/src/components/history/historyItem.js
@@ -16,23 +16,3 @@ const HistoryItem = (props) => {
 };
 
 export default HistoryItem;
-
-const HistoryItems = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 70%;
-  margin: auto;
-  background: rgba(245, 245, 245, 1);
-  border-radius: 6px;
-  padding: 0 2rem;
-  font-size: 1rem;
-  color: black !important;
-
-  &:not(:last-of-type) {
-    margin-bottom: 1rem;
-  }
-  &.active {
-    background: rgba(240, 252, 248, 1);
-  }
-`;
