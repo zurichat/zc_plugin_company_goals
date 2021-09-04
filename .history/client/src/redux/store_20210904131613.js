0ffb854155348a93acc5e49976824275e2762d0a
smit1298 <61621188+smit1298@users.noreply.github.com>
2021-09-04 13:16:39 +0100

    store

--- .history/client/src/redux/store_20210904131613.js
+++ .history/client/src/redux/store_20210904131613.js
@@ -1,17 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import goalReducer from '../features/goalSlice';
-
 import editMissionReducer from './showEditMissionModal';
-
 import editVisionReducer from './showEditVisionModal';
 
+
 const store = configureStore({
-  reducer: {
-    showMission: editMissionReducer,
-    showVision: editVisionReducer,
-    goal: goalReducer,
-  },
+    reducer: {
+        showMission: editMissionReducer,
+        showVision: editVisionReducer,
+    },
 });
 
 export default store;
