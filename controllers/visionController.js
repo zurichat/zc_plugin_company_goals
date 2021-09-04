3c55d2a40e165d33a29ba9dd6f633d6e5e19701a
Oluwasanmi <42076983+Dukesanmi@users.noreply.github.com>
2021-09-04 01:21:19 +0100

    Create visionController.js

---
+++ controllers/visionController.js
@@ -0,0 +1,22 @@
+// this module is used to handle the vision
+
+// request to get the mission
+exports.getVision = (req, res) => {
+  // get the vision
+  // fetch(vision from database)
+
+  // send the mission
+  res.send('This is a dummy vision');
+};
+
+exports.addVision = (req, res)=> {
+  // get the vision from client via req.body
+  const { vision } = req.body;
+  // insert the vision into the database
+} 
+
+exports.editVision = (req, res)=> {
+  // get the new vision from client via req.body
+  const { vision } = req.body;
+  // find and update the vision in the database with the edited vision statement   
+} 
