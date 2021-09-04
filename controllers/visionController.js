18f5384134b8ddaf3155595372519e43596240d5
Oluwasanmi <42076983+Dukesanmi@users.noreply.github.com>
2021-09-04 23:52:55 +0100

    Update visionController.js

--- controllers/visionController.js
+++ controllers/visionController.js
@@ -1,9 +1,15 @@
+const axios = require("axios");
 // this module is used to handle the vision
 
-// request to get the mission
-exports.getVision = (req, res) => {
-  // get the vision
+const collectionName = "vision"; 
+// request to get the vision
+exports.getVision = async(req, res) => {
+  const url = `https://test-zuri-core.herokuapp.com/crud/${collectionName}/`
   // fetch(vision from database)
+  const vision = await axios.get(url);
+  // Sending Responses
+  res.status(200).json({ status: 'success', data: { vision.data.body } }); 
+  
 
   // send the mission
   res.send('This is a dummy vision');
