b84798d3eef9831ae9ffa89a49bf7ed5a9f430a4
Triumph <Triumphjo123@gmail.com>
2021-09-04 11:24:46 +0100

    fixed conflicts

--- client/src/components/mainside/Mainside.jsx
+++ client/src/components/mainside/Mainside.jsx
@@ -1,15 +1,13 @@
 import styled from 'styled-components';
 
 import SimpleModal from '../createGoal/CreateGoal';
-
 import HistoryList from '../history/historyList';
 
 const Mainside = () => {
   return (
     <MainContainer>
       <Goal>
-        {' '}
-        <SimpleModal />{' '}
+        <SimpleModal />
       </Goal>
       <Goal primary>
         {/* goal tools like calendar, reports  .. go inside this component  */}
@@ -28,11 +26,14 @@ const MainContainer = styled.div`
 `;
 const Goal = styled.div`
   flex: 1;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding-top: 200px;
   background: red;
   height: 40vh;
   background: ${(props) => (props.primary ? 'white' : 'white')};
-  color: ${(props) => (props.primary ? 'black' : 'red')};
+  color: ${(props) => (props.primary ? 'white' : 'red')};
   margin: 10px;
   box-shadow: -2px 2px 3px rgba(0, 0, 0, 0.5);
-  overflow: hidden;
 `;
