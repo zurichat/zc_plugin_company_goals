6c6c008b0f5bebd61e8c8241e2a0eeb720079659
Teniola Fatunmbi <fatunmbiteniola001@gmail.com>
2021-09-04 18:49:21 +0100

    feat: added updateGoalByID function and route

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
-const { createGoal, getSingleGoal,getAllGoals, updateGoalByID } = require('../controllers/goalController');
+const { createGoal, getSingleGoal,getAllGoals, updateGoalByID, createGoals } = require('../controllers/goalController');
 
 const router = Router();
 
 router.route('/').post(createGoal).get(getAllGoals);
+router.route('/').post(createGoals);
 
 router.route('/:id').get(getSingleGoal)
 router.patch('/update/:id', updateGoalByID);
