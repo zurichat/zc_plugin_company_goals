a6faa2fa261deac3902f95c0cf86e74109703af3
Ojo Triumph <ojotriumph4@gmail.com>
2021-09-04 12:08:35 +0100

    used styled component instead of css modules

--- client/src/components/history/historyItem.js
+++ client/src/components/history/historyItem.js
@@ -1,21 +1,38 @@
 /* eslint-disable react/destructuring-assignment */
+import styled from 'styled-components';
 import React from 'react';
-
-import classes from './historyItem.module.css';
 
 const HistoryItem = (props) => {
   // eslint-disable-next-line react/destructuring-assignment
   // eslint-disable-next-line react/prop-types
   const historyContainer = props.history.map((element) => {
-    const historyItemClasses = !element.latestVisited ? `${classes.row}` : `${classes.row} ${classes.active}`;
     return (
-      <div className={historyItemClasses}>
+      <HistoryItems>
         <span>{element.timeVisited}</span>
         <h3>{element.locationLastVisited}</h3>
-      </div>
+      </HistoryItems>
     );
   });
-  return <>{historyContainer}</>;
 };
 
 export default HistoryItem;
+
+const HistoryItems = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 70%;
+  margin: auto;
+  background: rgba(245, 245, 245, 1);
+  border-radius: 6px;
+  padding: 0 2rem;
+  font-size: 1rem;
+  color: black !important;
+
+  &:not(:last-of-type) {
+    margin-bottom: 1rem;
+  }
+  &.active {
+    background: rgba(240, 252, 248, 1);
+  }
+`;
