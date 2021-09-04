4ef7866d7c15edb3ed390edbebe4676a5c0a9f91
chuksdozie <chuksdozie48@gmail.com>
2021-09-04 10:43:30 +0100

    update on the home conflict

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,8 @@
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-
-import AppHeader from '../components/header/Header';
 import Header from '../components/header/Header';
 import Mainside from '../components/mainside/Mainside';
-import EditMission from '../components/Modal/EditMission';
-import EditVision from '../components/Modal/EditVision';
 import Nav from '../components/navbar/Navbar';
-import GetGoals from '../components/getGoals/getGoals';
+import Goals from '../components/getGoals/getGoals';
 
 function App() {
   return (
@@ -15,23 +10,12 @@ function App() {
       <PluginSide>
         <Nav />
         <Header />
-
-        <GetGoals />
-
-        <Mainside />
- 
-        {/* <Deletemodal /> this is rendering above components..whoever is to work on it, should render it conditionally */}
-        <EditMission />
-        <EditVision />
       </PluginSide>
-     
+      <Mainside />
+      <Goals />
     </div>
   );
 }
-
-App.propTypes = {
-  onSearch: PropTypes.func.isRequired,
-};
 
 export default App;
 
