5a5c02a58ba864fd9374fa4e531d9cfec31866b8
smit1298 <61621188+smit1298@users.noreply.github.com>
2021-09-04 12:59:05 +0100

    editfrom

--- .history/client/src/components/EditGoal/EditGoalForm_20210904125835.js
+++ .history/client/src/components/EditGoal/EditGoalForm_20210904125835.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 import CheckOutlinedIcon from '@material-ui/icons/CheckOutlined';
 
-import { useSelector } from 'react-redux';
-
-import { selectGoal } from '../../features/goalSlice';
-
 import {
   Goal,
   Form,
@@ -24,11 +20,8 @@ import img from './icon/active.png';
 import lock from './icon/default.png';
 import people from './icon/Group 2684.png';
 
-import { ownerDocument } from '@material-ui/core';
-
 const EditGoalForm = React.forwardRef((props) => {
   // eslint-disable-next-line react/prop-types
-  const { name, owner, status, endDate, category, description } = useSelector(selectGoal);
   const { handleClose } = props;
   return (
     <Goal>
@@ -49,7 +42,7 @@ const EditGoalForm = React.forwardRef((props) => {
               <Info textColor="#999999">
                 Goals are high level containers that can be broken down into smaller target. Learn more
               </Info>
-              <Input type="text" id="name" value={name} />
+              <Input type="text" id="name" />
             </label>
             <Box>
               <Button type="Button" buttonPadding="0.625rem 0.875rem" borderRadius="3px">
@@ -73,7 +66,7 @@ const EditGoalForm = React.forwardRef((props) => {
                 <Title titleColor="#393939">Owner</Title>
                 <Info textColor="#999999">This is optional,who will take responsibility for the goals</Info>
               </div>
-              <Input type="text" id="name" placeholder="Mark Essien" value={owner} />
+              <Input type="text" id="name" placeholder="Mark Essien" />
             </label>
 
             <Box>
@@ -97,32 +90,17 @@ const EditGoalForm = React.forwardRef((props) => {
               <div>
                 <Title titleColor="#393939">Who has access to this goal?</Title>
                 <div style={{ marginTop: '2rem' }}>
-                  {status === true ? (
-                    <>
-                      <AccessButton active>
-                        <img src={people} alt="" />
-                        <Title active> Zuri&apos;s workspace</Title>
-                      </AccessButton>{' '}
-                      <AccessButton>
-                        <img src={lock} alt="" />
-                        <Title> Private</Title>
-                      </AccessButton>
-                    </>
-                  ) : (
-                    <>
-                      <AccessButton>
-                        <img src={people} alt="" />
-                        <Title> Zuri&apos;s workspace</Title>
-                      </AccessButton>{' '}
-                      <AccessButton active>
-                        <img src={lock} alt="" />
-                        <Title active> Private</Title>
-                      </AccessButton>
-                    </>
-                  )}
+                  <AccessButton borderDetails="1px solid #00b87c">
+                    <img src={people} alt="" />
+                    <Title titleColor="#00b87c"> Zuri&apos;s workspace</Title>
+                  </AccessButton>
+                  <AccessButton borderDetails="1px solid #999999">
+                    <img src={lock} alt="" />
+                    <Title> Private</Title>
+                  </AccessButton>
                 </div>
               </div>
-              <Input type="text" id="name" value={name} />
+              <Input type="text" id="name" />
             </label>
 
             <Box>
@@ -156,7 +134,6 @@ const EditGoalForm = React.forwardRef((props) => {
                 // eslint-disable-next-line no-return-assign
                 onBlur={(e) => (e.currentTarget.type = 'text')}
                 placeholder="End Date"
-                value={endDate}
               />
             </label>
 
@@ -184,7 +161,7 @@ const EditGoalForm = React.forwardRef((props) => {
                   This is optional. A categorization that will help in sorting from multiples{' '}
                 </Info>
               </div>
-              <Input type="text" id="category" placeholder="Product Design" value={category} />
+              <Input type="text" id="category" placeholder="Product Design" />
             </label>
 
             <Box>
@@ -212,7 +189,7 @@ const EditGoalForm = React.forwardRef((props) => {
                   This is optional. A short explanation on why the goal is set and how it can be achieved
                 </Info>
               </div>
-              <Input type="text" id="name" value={description} />
+              <Input type="text" id="name" />
             </label>
 
             <Box>
