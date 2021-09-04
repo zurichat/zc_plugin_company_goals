3c8594b507f4e36a2b7519978c3069f2d1a0058a
Ojo Triumph <ojotriumph4@gmail.com>
2021-09-04 17:22:43 +0100

    Update historyList.js

--- client/src/components/history/historyList.js
+++ client/src/components/history/historyList.js
@@ -4,7 +4,9 @@ import styled from 'styled-components';
 
 import HistoryItem from './historyItem';
 
-import {Row} from './Item';
+import {Row, History} from './Item';
+
+import {Goal} from '../mainside/mainside.styled.js';
 
 const history = [
   {
@@ -28,13 +30,16 @@ const HistoryList = () => {
   // Note: button will be changed to link after importing it from 'react-router-dom'
 
   return (
-    <div>
+    <History>
+<Goal>
       <Row>
         <h3>History</h3>
         <Button>View all</Button>
       </Row>
       <HistoryItem history={history} />
-    </div>
+</Goal>
+
+    </History>
   );
 };
 
