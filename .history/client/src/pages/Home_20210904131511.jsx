0644772f126d6bae2a20a1fea4d5981278751821
smit1298 <61621188+smit1298@users.noreply.github.com>
2021-09-04 13:15:35 +0100

    home

--- .history/client/src/pages/Home_20210904131511.jsx
+++ .history/client/src/pages/Home_20210904131511.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+
 import AppHeader from '../components/header/Header';
 import Header from '../components/header/Header';
 import Mainside from '../components/mainside/Mainside';
@@ -12,11 +13,14 @@ function App() {
     <div>
       <PluginSide>
         <Nav />
+        <Header />
         <Mainside />
+        
         {/* <Deletemodal /> this is rendering above components..whoever is to work on it, should render it conditionally */}
         <EditMission />
         <EditVision />
       </PluginSide>
+     
     </div>
   );
 }
