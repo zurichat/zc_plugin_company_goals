ec3c59c0b81a13aa1120a0dd57d71a9056460e7d
Abdulrahman Yusuf <abdulrahmanyusuf125@gmail.com>
2021-09-04 20:06:57 +0100

    add ajv validation middleware

---
+++ middleware/validate.js
@@ -0,0 +1,35 @@
+const AppError = require('../utils/appError');
+const Ajv = require('ajv');
+
+const ajv = new Ajv();
+
+const validate = (options = {}) => {
+  return (req, res, next) => {
+    try {
+      // validate request body
+      if (options.body) {
+        if (!ajv.validate(options.body, req.body)) {
+          throw ajv.errors;
+        }
+      }
+
+      // Validate request parameters
+      if (options.params) {
+        if (!ajv.validate(options.params, req.params)) {
+          throw ajv.errors;
+        }
+      }
+
+      // Validate request query
+      if (options.query) {
+        if (!ajv.validate(options.query, req.query)) {
+          throw ajv.errors;
+        }
+      }
+    } catch (e) {
+      next(e);
+    }
+  };
+};
+
+module.exports = validate;
