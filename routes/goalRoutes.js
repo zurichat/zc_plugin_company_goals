a23059e6ea354d52a03596507554d865e5c9f988
Tito <ebenirot@gmail.com>
2021-09-05 17:13:43 +0100

    Made minor changes and resolved branch conflicts

--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const { getSingleGoal, getAllGoals, updateSingleGoalById, createGoals, getArchivedGoals } = require('../controllers/goalController');
+const { getSingleGoal, getAllGoals, createGoals, updateSingleGoalById, getArchivedGoals } = require('../controllers/goalController');
 
 const router = Router();
 
@@ -10,6 +10,6 @@ router.route('/archived').get(getArchivedGoals)
 
 router.route('/:id').get(getSingleGoal);
 
-router.patch('/update/:id', updateSingleGoalById);
+router.put('/update/:id', updateSingleGoalById);
 
 module.exports = router;
