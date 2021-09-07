ad7f165e3bcd2642919532c8eb8959de63efffa7
Tunde Alabi <tundealabi4780@gmail.com>
2021-09-07 04:39:55 +0100

    closes #613; modal for editing vision is now fully responsive and functional

---
+++ client/src/components/organization_vision/org_edit_vision/modal/EditOrgVisionModal.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { saveVision, showEditVisionModal } from 'redux/organizationVision.slice';
+
+import {
+  EditVisionModal,
+  EditVisionContainer,
+  Header,
+  TextBox,
+  ActionButtonsContainer,
+  ActionButton,
+  ActionCancelEditVisionButton,
+} from './EditOrgVision.styled';
+
+const OrganizationVisionEditModal = () => {
+  const dispatch = useDispatch();
+  const showVisionModal = useSelector(({ organizationVision }) => organizationVision.showVisionModal);
+  const [editText, setEditText] = useState('');
+
+  return (
+    <EditVisionModal
+      aria-labelledby="organization-vision-modal"
+      aria-describedby="edit-organization-vision-modal"
+      open={showVisionModal}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Fade in={showVisionModal}>
+        <EditVisionContainer>
+          <Header id="transition-modal-title">Edit Vision</Header>
+          <TextBox placeholder="Click to edit..." value={editText} onChange={(e) => setEditText(e.target.value)} />
+          <ActionButtonsContainer>
+            <ActionCancelEditVisionButton onClick={() => dispatch(showEditVisionModal())}>
+              Cancel
+            </ActionCancelEditVisionButton>
+            <ActionButton onClick={() => editText && dispatch(saveVision(editText))}>Save</ActionButton>
+          </ActionButtonsContainer>
+        </EditVisionContainer>
+      </Fade>
+    </EditVisionModal>
+  );
+};
+
+export default OrganizationVisionEditModal;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import deleteGoalReducer from './deleteGoal.slice';
+import newGoalReducer from './newGoalSlice';
 import visionReducer from './organizationVision.slice';
 import editMissionReducer from './showEditMissionModal';
 import createGoalModalReducer from './toggleCreateGoalModal.slice';
 import editGoalModalReducer from './toggleEditGoalModal.slice';
-import newGoalReducer from './newGoalSlice';
 
 const store = configureStore({
   reducer: {
