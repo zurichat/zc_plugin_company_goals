854398beb1fce63470653a0bbd91966b9fc6a5db
kelvinakpobome <kelvinakpobome@gmail.com>
2021-09-08 17:22:10 +0100

    added sidebar check

--- controllers/sidebarController.js
+++ controllers/sidebarController.js
@@ -2,10 +2,12 @@ const sidebarOptions = require('../data/sidebarPopulate');
 const catchAsync = require('../utils/catchAsync');
 
 
+
 const readSidebar = catchAsync(async (req, res, next) => {
-
-  
-  return res.status(200).json(sidebarOptions);
+  if (req.query.userID =='1234' && req.query.token == '61330fcfbfba0a42d7f38e59'){
+    return res.status(200).json(sidebarOptions);
+  }else{
+    return res.status(403).send({ message: 'error', data: "bad request"});
+  }
 });
-
 module.exports = readSidebar;
