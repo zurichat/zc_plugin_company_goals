5df1fa6a8cdac1e45105bc7c7f96cc2bbcb4cf35
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-09-09 14:34:06 +0100

    add title field to rooms

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -5,21 +5,24 @@ const {insertOne} = require("../db/databaseHelper");
 
 const roomSchema = Joi.object({
     id:Joi.string().required().messages({
-        "any.required":"uuid of the rom is required"
+        "any.required":"uuid of the room is required"
+    }),
+    title:Joi.string().required().messages({
+        "any.required":"title of the room is required"
     }),
     organization_id:Joi.string().required().messages({
         "any.required":"organization id is required"
-    })
+    }),
 })
 
 exports.createRoom = catchAsync(async (req, res, next) => {
-    const {organization_id} = req.query;
+    const {organization_id,title} = req.query;
     id = uuidv4();
 
     // Validate the body
-    await roomSchema.validateAsync({id, organization_id});
+    await roomSchema.validateAsync({id, organization_id,title});
 
-    const room = await insertOne("rooms", {id, organization_id, users: []});
+    const room = await insertOne("rooms", {id, organization_id,title,users: []});
 
     res.status(201).json({
         status: "success",
