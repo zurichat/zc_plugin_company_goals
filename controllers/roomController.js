c541581a85ab39d943388872a8116dd65087772a
Samuel Anozie <samuelanozie10@yahoo.com>
2021-09-09 18:37:27 +0100

    feat: join room controller

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -29,3 +29,29 @@ exports.createRoom = catchAsync(async (req, res, next) => {
         data: room.data
     })
 });
+
+const userSchema = Joi.object({
+  room_id: Joi.string().required().messages({
+    'any.required': 'room id is required',
+  }),
+  organization_id: Joi.string().required().messages({
+    'any.required': 'organization id is required',
+  }),
+  user_id: Joi.string().required().messages({
+    'any.required': 'user id is required',
+  }),
+});
+
+exports.joinRoom = catchAsync(async (req, res, next) => {
+  const { room_id, organization_id, user_id } = req.query;
+
+  // Validate the body
+  await userSchema.validateAsync({ room_id, organization_id, user_id });
+
+  const roomuser = await insertOne('roomusers', { room_id, user_id });
+
+  res.status(201).json({
+    status: 'success',
+    data: roomuser.data,
+  });
+});
