042dc84ef1ba233c869c966a413dd9cd941d545b
adepeju4 <adepeju.kayode@gmail.com>
2021-09-12 01:27:28 +0100

    refactor(BE): updated goal schema

--- schemas/index.js
+++ schemas/index.js
@@ -46,7 +46,6 @@ exports.goalsSchema = Joi.object({
   currency_unit: Joi.string().optional(),
   start: Joi.alternatives().try(Joi.string(), Joi.number()).optional(),
   target: Joi.alternatives().try(Joi.string(), Joi.number()).optional(),
-<<<<<<< HEAD
   milestone1: Joi.alternatives().try(Joi.number(), Joi.string()).optional(),
   milestone2: Joi.alternatives().try(Joi.number(), Joi.string()).optional(),
   milestone3: Joi.alternatives().try(Joi.number(), Joi.string()).optional(),
@@ -55,15 +54,6 @@ exports.goalsSchema = Joi.object({
     .valid(`Low`, `Medium`, `High`, `Intermediate`)
     .required()
     .messages({ 'any.required': 'goal priority must be inputed' })
-=======
-  milestone: Joi.string().valid(`number`, `currency`).optional(),
-  milestone_start: Joi.alternatives().try(Joi.string(), Joi.number()).optional(),
-  milestone_target: Joi.alternatives().try(Joi.string(), Joi.number()).optional(),
-  goal_priority: Joi.string()
-    .valid(`Low`, `Medium`, `High`, `Intermediate`)
-    .required()
-    .messages({ 'any.required': 'goal priority must be inputed' }),
->>>>>>> 8157db22d8785976071981821a92c5c2f5f69e4c
 });
 
 // mission schema
