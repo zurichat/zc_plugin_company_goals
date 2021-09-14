d8ddee67d49c71b4a48d5c81f16661c0401936d3
Triumph <Triumphjo123@gmail.com>
2021-09-14 17:41:04 +0100

    resolved conflicts

--- client/src/components/mainside/Mainside.jsx
+++ client/src/components/mainside/Mainside.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import EmptyGoal from 'components/empty-goal-interface/EmptyGoal';
 import InnerNav from 'components/goal_interface_inner_header/InnerNav';
 // import GetGoals from '../getGoals/getGoals';
+import GoalsNavLayout from '../goal_interface_navbar/NavLayout';
 import GoalItem from '../Goals/GoalItem';
 import GoalsNavLayout from '../goal_interface_navbar/NavLayout';
 import Goals from '../Goals/Goals';
