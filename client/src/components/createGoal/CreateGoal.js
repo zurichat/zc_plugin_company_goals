fe2573f7918174ca15d14da03bbdc32ec24c7442
abiona01 <quadratwemimo@gmail.com>
2021-09-18 01:25:06 +0100

    goal form redesign

--- client/src/components/createGoal/CreateGoal.js
+++ client/src/components/createGoal/CreateGoal.js
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch } from 'react-redux';
 // useSelector
 import { toggleCreateGoalModalAction } from '../../redux/toggleCreateGoalModal.slice';
 import GoalForm from './GoalForm';
-import { GlobalStyles } from './GoalForm.style';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -19,8 +17,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SimpleDialog() {
   const classes = useStyles();
-  const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
   const dispatch = useDispatch();
   // const showCreateGoalModal = useSelector(({ toggleCreateGoalModal }) => toggleCreateGoalModal.showCreateGoalModal);
 
@@ -41,13 +38,10 @@ export default function SimpleDialog() {
         // open={showCreateGoalModal}
         open={open}
         onClose={handleClose}
-        maxWidth="sm"
-        fullScreen={fullScreen}
-        fullWidth
+        maxWidth="xs"
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
-        <GlobalStyles />
         <GoalForm className={classes.paper} handleClose={handleClose} />
       </Dialog>
     </>
