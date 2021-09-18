cb0dddc782246f94d044e7a838b6eeac8600253c
temilade05 <temioladugba@gmail.com>
2021-09-18 12:56:39 +0100

    add logging to databaseHelper.js

--- db/databaseHelper.js
+++ db/databaseHelper.js
@@ -4,6 +4,9 @@ const AppError = require('../utils/appError');
 const { URL, payload } = require('../utils/config').DATABASE;
 
 exports.insertOne = async (collectionName, data, organization_id) => {
+
+  console.log(`Insert one operation to ${collectionName} of ${organization_id} started. Writing the following ${data}`);
+
   try {
     const newPayload = { ...payload };
     newPayload.collection_name = collectionName;
@@ -11,13 +14,18 @@ exports.insertOne = async (collectionName, data, organization_id) => {
     newPayload.organization_id = organization_id;
 
     const response = await axios.post(`${URL}/write`, newPayload);
+    console.log(`The write operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The write operation failed with the following error messages: ${error}`);
     throw new AppError(`Insert One operation failed: ${error}`, 500);
   }
 };
 
 exports.insertMany = async (collectionName, data, organization_id) => {
+
+  console.log(`Insert many operation to ${collectionName} of ${organization_id} started. Writing the following ${data}`);
+
   try {
     payload.collection_name = collectionName;
     payload.payload = data;
@@ -25,35 +33,50 @@ exports.insertMany = async (collectionName, data, organization_id) => {
     payload.organization_id = organization_id;
 
     const response = await axios.post(`${URL}/write`, payload);
+    console.log(`The write operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
-    throw new AppError(`Insert One operation failed: ${error}`, 500);
+    console.log(`The write operation failed with the following error messages: ${error}`);
+    throw new AppError(`Insert many operation failed: ${error}`, 500);
   }
 };
 
 exports.findAll = async (collectionName, organization_id) => {
+
+  console.log(`Find all operation for ${collectionName} of ${organization_id} started.`);
+
   try {
     const { plugin_id } = payload;
 
     const response = await axios.get(`${URL}/read/${plugin_id}/${collectionName}/${organization_id}`);
+    console.log(`The read operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The read operation failed with the following error messages: ${error}`);
     throw new AppError(`find all operation failed: ${error}`, 500);
   }
 };
 
 exports.findById = async (collectionName, id, organization_id) => {
+
+  console.log(`Find by id operation for ${collectionName} of ${organization_id} started. The id is ${id}.`);
+
   try {
     const { plugin_id } = payload;
 
     const response = await axios.get(`${URL}/read/${plugin_id}/${collectionName}/${organization_id}?_id=${id}`);
+    console.log(`The read operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The read operation failed with the following error messages: ${error}`);
     throw new AppError(`find by id operation failed: ${error}`, 500);
   }
 };
 
 exports.find = async (collectionName, filter, organization_id) => {
+
+  console.log(`Find operation for ${collectionName} of ${organization_id} started. The specified filter is ${filter}.`);
+
   try {
     const { plugin_id } = payload;
 
@@ -67,13 +90,19 @@ exports.find = async (collectionName, filter, organization_id) => {
     url = url.substr(0, url.length - 1);
 
     const response = await axios.get(url);
+    console.log(`The read operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The read operation failed with the following error messages: ${error}`);
     throw new AppError(`find by id operation failed: ${error}`, 500);
   }
 };
 
 exports.updateOne = async (collectionName, data, filter, organization_id, id = null) => {
+
+  console.log(`Update one operation in ${collectionName} of ${organization_id} started. The specified filter is 
+  ${filter} & would update all successful matches with the following data: ${data}.`);
+
   try {
     payload.collection_name = collectionName;
     payload.payload = data;
@@ -82,13 +111,19 @@ exports.updateOne = async (collectionName, data, filter, organization_id, id = n
     payload.organization_id = organization_id;
 
     const response = await axios.put(`${URL}/write`, payload);
+    console.log(`The update operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The update operation failed with the following error messages: ${error}`);
     throw new AppError(`Update One operation failed: ${error}`, 500);
   }
 };
 
 exports.updateMany = async (collectionName, data, filter, organization_id) => {
+
+  console.log(`Update many operation in ${collectionName} of ${organization_id} started. The specified filter is 
+  ${filter} & would update all successful matches with the following data: ${data}.`);
+
   try {
     payload.collection_name = collectionName;
     payload.payload = data;
@@ -97,13 +132,19 @@ exports.updateMany = async (collectionName, data, filter, organization_id) => {
     payload.organization_id = organization_id;
 
     const response = await axios.put(`${URL}/write`, payload);
+    console.log(`The update operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The update operation failed with the following error messages: ${error}`);
     throw new AppError(`Update One operation failed: ${error}`, 500);
   }
 };
 
 exports.deleteOne = async (collectionName, organization_id, _id) => {
+
+  console.log(`Delete one operation in ${collectionName} of ${organization_id} started. The operation would delete the 
+  document with id: ${_id}`);
+
   try {
     payload.collection_name = collectionName;
     payload.object_id = _id;
@@ -114,13 +155,18 @@ exports.deleteOne = async (collectionName, organization_id, _id) => {
       url: `${URL}/delete`,
       data: payload,
     });
+
+    console.log(`The delete operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The delete operation failed with the following error messages: ${error}`);
     throw new AppError(`Delete one operation failed: ${error}`, 500);
   }
 };
 
 exports.deleteMany = async (collectionName, filter, organization_id) => {
+  console.log(`Delete many operation in ${collectionName} of ${organization_id} started. The operation would delete all 
+  documents that match the filter: ${filter}`);
   try {
     payload.collection_name = collectionName;
     payload.filter = filter;
@@ -133,8 +179,10 @@ exports.deleteMany = async (collectionName, filter, organization_id) => {
       data: payload,
     });
 
+    console.log(`The delete operation was successful with the following response: ${response.data.data}`);
     return response;
   } catch (error) {
+    console.log(`The delete operation failed with the following error messages: ${error}`);
     throw new AppError(`Update One operation failed: ${error}`, 500);
   }
 };
