d04e0df6fa87c8efe919052335b9b5ca9cde7b8c
Adeniyii <ifedayoadeniyi@gmail.com>
2021-09-19 14:28:15 +0100

    refactor: remove validation middleware

--- routes/visionRoutes.js
+++ routes/visionRoutes.js
@@ -1,18 +1,12 @@
 const { Router } = require('express');
 const { publish } = require('../controllers/centrifugoController');
 const { getVision, updateVision } = require('../controllers/visionController');
-const { verifyToken, requireRoles, checkIsValidUser } = require('../middlewares/validate');
+// const { verifyToken, requireRoles, checkIsValidUser } = require('../middlewares/validate');
 
 const router = Router();
 
-router.get('/:organization_id', verifyToken, checkIsValidUser, requireRoles(['admin', 'user', 'owner']), getVision);
-router.patch(
-  '/:organization_id',
-  verifyToken,
-  checkIsValidUser,
-  requireRoles(['admin', 'owner', 'user']),
-  updateVision
-);
+router.get('/:organization_id', getVision);
+router.patch('/:organization_id', updateVision);
 
 // DO NOT TOUCH (TESTING)
 router.post('/', async (req, res) => {
