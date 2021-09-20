f313a56eaff8e8d5a1cd783038ec6ffcf84f06f6
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-09-20 06:52:41 +0100

    resolve issues on backend and fronetend

--- client/src/components/mainside/Mainside.jsx
+++ client/src/components/mainside/Mainside.jsx
@@ -10,7 +10,7 @@ import MobilePrivate from '../../components/Dropdown/MobilePrivate';
 import InnerNav from '../../components/goal_interface_inner_header/InnerNav';
 // import GetGoals from '../getGoals/getGoals';
 // eslint-disable-next-line import/no-unresolved
-import ExportReport from 'components/Modal/ExportReport';
+//import ExportReport from 'components/Modal/ExportModal/ExportReport';
 import { getGoals } from '../../redux/showGoalSlice';
 import GoalsNavLayout from '../goal_interface_navbar/NavLayout';
 // import GoalItem from '../Goals/GoalItem';
