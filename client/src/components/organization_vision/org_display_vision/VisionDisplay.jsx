74aa56fd361c6f19420f123b18bec064624f7996
Teniola Fatunmbi <fatunmbiteniola001@gmail.com>
2021-09-20 19:05:31 +0100

    feat: used the vision state for the vision text

--- client/src/components/organization_vision/org_display_vision/VisionDisplay.jsx
+++ client/src/components/organization_vision/org_display_vision/VisionDisplay.jsx
@@ -15,7 +15,7 @@ const DisplayOrganizationVision = () => {
         Vision
       </Title>
       <Box className="box">
-        <VisionField className="visionInput">{vision || 'Click to add a vision'}</VisionField>
+        <VisionField className="visionInput">{vision || 'Click to add a vision'} </VisionField>
         <Editbutton className="editbutton" right="7%" rsRight="16%" onClick={() => dispatch(showEditVisionModal())}>
           <img src={editImg} alt="edit" />
         </Editbutton>
