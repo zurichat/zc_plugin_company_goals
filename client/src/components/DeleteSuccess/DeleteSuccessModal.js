2f51f6e315222a134f1ab80043081a3355ff5e22
taiwo truth <taiwotruth@gmail.com>
2021-09-21 20:09:08 +0100

    GoalDelete completely updated

--- client/src/components/DeleteSuccess/DeleteSuccessModal.js
+++ client/src/components/DeleteSuccess/DeleteSuccessModal.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-
+import { getGoals } from '../../redux/showGoalSlice';
 import { deleteSuccessAction } from '../../redux/deleteGoal.slice';
 import { FadedBg, DeleteContent, DeleteHead, DeleteInfo, DeleteModal, DelButton, XBtn, Images } from '../Deletemodal/Delete.styled';
 import deleteSuccessImg from './deleteSuccess';
@@ -7,6 +7,13 @@ import deleteSuccessImg from './deleteSuccess';
 const DeleteSuccess = () => {
   const dispatch = useDispatch();
   const showDeleteSuccesslModal = useSelector(({ deleteGoal }) => deleteGoal.showDeleteSuccessModal);
+
+  const updateGoal=()=>{
+    dispatch(getGoals());
+    dispatch(deleteSuccessAction());
+  }
+
+
   return (
     <FadedBg className="faded" show={showDeleteSuccesslModal}>
       <DeleteModal>
@@ -17,7 +24,7 @@ const DeleteSuccess = () => {
         <DeleteContent>
           <DeleteHead className="delete-head">Goal Deleted Successfully </DeleteHead>
           <DeleteInfo>Clicking the return button to go back to the main dashboard.</DeleteInfo>
-          <DelButton type="submit">Ok</DelButton>
+          <DelButton type="submit" onClick={updateGoal}>Ok</DelButton>
         </DeleteContent>
       </DeleteModal>
     </FadedBg>
