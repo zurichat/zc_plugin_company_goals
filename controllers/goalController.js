33d367b8e82582d15b51bd8b7a25dfbbc3d6388a
adepeju4 <adepeju.kayode@gmail.com>
2021-09-22 00:50:28 +0100

    refactor(BE): added logger info

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -36,11 +36,14 @@ exports.getAllGoals = catchAsync(async (req, res, next) => {
   try {
     const goals = await findAll('goals', orgId);
 
+    logger.info(`If the response we get is null or the goals length is less than 1, we are 
+    returning an empty array`);
     if (goals.data.data === null || goals.data.data.length < 1) {
       return res.status(200).json({ message: 'success', data: [] });
     }
 
-    if (goals.data.status === 200) {
+    logger.info(`If the response has a status code of 200 and the goals exist in the array, we return the goals array`);
+    if (goals.data.status === 200 && goals.data.data.length > 1) {
       return res.status(200).json({ status: 200, message: 'success', data: goals.data.data });
     }
   } catch (error) {
