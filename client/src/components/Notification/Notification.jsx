424f222e5408442823f3cfb4d892ab03d3b3b8d3
aycom366 <bamigboyeayomide200@gmail.com>
2021-09-23 12:48:06 +0100

    Adjusted Font family

--- client/src/components/Notification/Notification.jsx
+++ client/src/components/Notification/Notification.jsx
@@ -15,11 +15,10 @@ import {
   Paragraph,
   Button,
   Grid,
-  Section,
   FlexRows,
-  FlexRow,
 } from './styledNotification';
 import { useDispatch } from 'react-redux';
+
 import {
   deleteNotificationAsync,
   markAllNotificationsAsReadAsync,
@@ -28,11 +27,27 @@ import {
 } from '../../redux/notificationSlice';
 import { useSelector } from 'react-redux';
 
+const getWindowDimension = () => {
+  const { innerWidth: width } = window;
+  console.log(width);
+  return width;
+};
+
+getWindowDimension();
+
 function Notification() {
   const [isActive, setIsActive] = useState(false);
   const [index, setIndex] = useState(0);
   const notifications = useSelector(selectNotifications);
   const dispatch = useDispatch();
+  const [getWidth, setGetWidth] = useState(getWindowDimension);
+
+  //getting current window width to display time stamps below or sidesways
+  useEffect(() => {
+    const handleResize = () => setGetWidth(getWindowDimension);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [getWidth]);
 
   const handleMarkAllAsRead = () => {
     dispatch(markAllNotificationsAsReadAsync());
@@ -46,11 +61,9 @@ function Notification() {
     dispatch(deleteNotificationAsync({ id: _id }));
   };
 
-  const handleAccordion = (e, _id) => {
-    if (e.target.name !== 'create-wireframe') {
-      setIsActive(!isActive);
-      setIndex(_id);
-    }
+  const handleAccordion = (_id) => {
+    setIsActive(!isActive);
+    setIndex(_id);
   };
 
   return (
@@ -77,33 +90,44 @@ function Notification() {
                   <Grid key={_id} gridActive={_id === index} borderBottom>
                     <MailOutlineIcon style={{ margin: '0 auto', color: '#999999' }} />
                     <FlexColumn>
-                      <FlexRows onClick={(e) => handleAccordion(e, _id)} Rows goalachievedTimeline>
+                      <FlexRows onClick={() => handleAccordion(_id)} Rows goalachievedTimeline>
                         <FlexColumn flexBasicsColumn>
                           <Paragraph style={{ marginBottom: '10px' }} goalParagraphHeader>
                             {header}
                           </Paragraph>
-                          <Button status colour={colour} name="create-wireframe" goalButtonHeaderWireframe>
+                          <Button colour={colour} goalButtonHeaderWireframe>
                             {goalName}
                           </Button>
                         </FlexColumn>
                         <FlexColumn arrowContainer>
                           {index === _id && isActive ? <ExpandMoreIcon /> : <ExpandLessIcon />}
-                          <Paragraph flexbasicsParagraph>
-                            <TimeAgo datetime={createdAt} />
-                          </Paragraph>
+
+                          {/* windows width is greater than 1300  */}
+                          {getWidth > 1300 && (
+                            <Paragraph flexbasicsParagraph>
+                              <TimeAgo datetime={createdAt} />
+                            </Paragraph>
+                          )}
                         </FlexColumn>
                       </FlexRows>
                       {index === _id && isActive && (
                         <FlexColumn moreNotificationInfo>
                           <Paragraph moreInfo>{description}</Paragraph>
-                          <FlexRows>
-                            <Button btnFunction onClick={() => handleDelete(_id)}>
+                          <FlexRows AlignRight>
+                            <Button style={{ marginRight: '10px' }} btnFunction onClick={() => handleDelete(_id)}>
                               Delete
                             </Button>
                             <Button btnFunction onClick={() => handleMarkAsRead(_id)}>
                               {isRead ? 'Mark as Unread' : 'Mark as Read'}
                             </Button>
                           </FlexRows>
+
+                          {/* windows width is less than 1300  */}
+                          {getWidth <= 1300 && (
+                            <Paragraph style={{ marginTop: '1rem' }} flexbasicsParagraph>
+                              <TimeAgo datetime={createdAt} />
+                            </Paragraph>
+                          )}
                         </FlexColumn>
                       )}
                     </FlexColumn>
