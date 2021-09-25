018aab7e4571264c332e18b3eee73078054f10f1
Alhazan Mubarak <olajuwonmubarak355@gmail.com>
2021-09-25 19:42:04 +0100

    fix the error component, and now render with dynamic errorMessage

--- client/src/components/error/Error.jsx
+++ client/src/components/error/Error.jsx
@@ -4,14 +4,14 @@ import { getGoals } from '../../redux/showGoalSlice';
 import Ellipse from './alert-octagon/active.svg';
 import { ErrorDiv, ImgContainer, ErrorText, ErrorButton } from './Error.styled.js';
 
-const Error = () => {
+const Error = ({ errorMessage }) => {
   const dispatch = useDispatch();
   return (
     <ErrorDiv>
       <ImgContainer>
         <img src={Ellipse} alt="error" />
       </ImgContainer>
-      <ErrorText>Error</ErrorText>
+      <ErrorText>{errorMessage.message}</ErrorText>
       <ErrorButton onClick={() => dispatch(getGoals())}>Retry</ErrorButton>
     </ErrorDiv>
   );
