c37385439231efb3f53d9f02a0d5f1206046570a
Ojo Triumph <ojotriumph4@gmail.com>
2021-09-29 23:07:23 +0100

    Update GoalItem.jsx

--- client/src/components/Goals/GoalItem.jsx
+++ client/src/components/Goals/GoalItem.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-underscore-dangle */
+import axios from 'axios'
 import { Container, Grid } from '@material-ui/core';
 import dislikes from '../../Images/png/dislikes.png';
 import likes from '../../Images/png/likes.png';
 import views from '../../Images/png/views.png';
+import { useEffect , useState } from 'react'
 import GoalDropDown from './GoalDropDown';
 import {
   useStyles,
@@ -17,10 +19,63 @@ import {
 } from './GoalItem.style';
 import { useSelector, useDispatch } from 'react-redux';
 import { addDisLike, addLike } from '../../redux/likeGoalSlice';
+import { useParams } from 'react-router';
+import { GetUserInfo } from '@zuri/control';
+
 
 
 
 const GoalItem = ({ goalData }) => { 
+  let { orgId } = useParams();
+  const userId = JSON.parse(sessionStorage.getItem('user'));
+  //Setting Likes and retriving
+  const [like, setLike] = useState('');
+  const [totalLikes, setTotalLikes] = useState(0);
+  useEffect(() => {
+    axios
+    .get(`https://goals.zuri.chat/api/v1/goals/goallikes?org_id=${orgId || '6145d099285e4a184020742e'}&goal_id=${goalData._id}`)  
+    .then(response => setTotalLikes(response.data.data.count))
+    .catch(error => console.log(error))
+
+  }, [like])
+
+  const handleSetLike = (e) => {
+    e.stopPropagation();
+
+    axios
+    .get(`https://goals.zuri.chat/api/v1/goals/like?org_id=${orgId || '6145d099285e4a184020742e'}&goal_id=${goalData._id}&user_id=${userId ? userId.id : 5}`)
+    .then(response => setLike(response.data.message))
+    .catch(error => console.log(error));
+    
+  }
+
+  //Setting Dislikes and retriving Dislikes
+  const [dislike, setDislike] = useState('');
+  const [totalDislikes, setTotalDislikes] = useState(0);
+
+  useEffect(() => {
+    axios
+    .get(`https://goals.zuri.chat/api/v1/goals/goaldislikes?org_id=${orgId || '6145d099285e4a184020742e'}&goal_id=${goalData._id}`)  
+    .then(response => setTotalDislikes(response.data.data.count))
+    .catch(error => console.log(error))
+
+  }, [dislike])
+
+  const handleSetDislike = (e) => {
+    e.stopPropagation();
+
+    axios
+    .get(`https://goals.zuri.chat/api/v1/goals/dislike?org_id=${orgId || '6145d099285e4a184020742e'}&goal_id=${goalData._id}&user_id=${userId ? userId.id : 5}`)
+    .then(response => setDislike(response.data.message))
+    .catch(error => console.log(error))  
+
+  }
+
+
+
+
+
+
 
   // const loop = (e) => {
   //   fetch('https://goals.zuri.chat/api/v1/goals/?org_id=6145d099285e4a184020742e&user_id=6145cf0c285e4a1840207426&goal_id=${e}')
@@ -30,7 +85,7 @@ const GoalItem = ({ goalData }) => {
   // }
 
   // loop(goalData.id)
-
+  // console.log("This is what i look up to",goalData._id)
 
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -53,48 +108,6 @@ const GoalItem = ({ goalData }) => {
   };
 
  
-  const goalLikes = useSelector((state) => state.likeGoals.likes);
-  const goalDislikes = useSelector((state) => state.likeGoals.dislikes);
-  // const errorMessage = useSelector((state) => state.goals.errorMessage);
-
-  const likeGoal = (e) => {
-    let goalID = goalData.id
-    e.stopPropagation();
-    let like = dispatch(addLike(1));
-    (async () => {
-      const rawResponse = await fetch('https://goals.zuri.chat/api/v1/goals/like?org_id=6145d099285e4a184020742e&user_id=6145cf0c285e4a1840207426&goal_id=${goalID}', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: {like}
-      });
-      const content = await rawResponse.json();
-    
-      
-    })();
-    
-}
-  
-  const disLikeGoal = (e) => {
-    let goalIDDislike = goalData.id
-    e.stopPropagation();
-    let dislike = dispatch(addDisLike(1));
-    (async () => {
-      const rawResponse = await fetch('https://goals.zuri.chat/api/v1/goals/dislike  ?org_id=6145d099285e4a184020742e&user_id=6145cf0c285e4a1840207426&goal_id=${goalIDDislike}', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: {dislike}
-      });
-      const content = await rawResponse.json();
-  
-    })();
-  };
-
   
 
   const Progress = ((goalData.milestone1 + goalData.milestone2 + goalData.milestone3) / 30) * 100;
@@ -125,13 +138,13 @@ const GoalItem = ({ goalData }) => {
       </Grid>
 
       <Grid item xs={12} sm={3} className={classes.icons}>
-        <IconItemContainer onClick={(ev) => likeGoal(ev)}>
+        <IconItemContainer onClick={(ev) => handleSetLike(ev)}>
           <img src={likes} alt="likes-icon" className={classes.iconImages} />
-          <IconItemCount>{goalLikes}</IconItemCount>
+          <IconItemCount>{totalLikes}</IconItemCount>
         </IconItemContainer>
-        <IconItemContainer onClick={(ev) => disLikeGoal(ev)}>
+        <IconItemContainer onClick={(ev) => handleSetDislike(ev)}>
           <img src={dislikes} alt="dislikes-icon" className={classes.iconImages} />
-          <IconItemCount>{goalDislikes}</IconItemCount>
+          <IconItemCount>{totalDislikes}</IconItemCount>
         </IconItemContainer>
       </Grid>
 
