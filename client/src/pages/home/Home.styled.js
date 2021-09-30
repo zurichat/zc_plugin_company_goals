b593da00a9f152180ab98b02c7a6d4f2e0320ce3
sticklo <mafimisebiadebayo@gmail.com>
2021-09-30 20:17:58 +0100

    fix-mobile-view

--- client/src/pages/home/Home.styled.js
+++ client/src/pages/home/Home.styled.js
@@ -57,6 +57,12 @@ cursor: pointer;
 export const Paragraph = styled.p`
 font-size:2rem;
 padding: 2rem;
+@media only screen and (max-width: 375px){
+
+text-align:center;
+font-size:1.6rem;
+
+}
 
 `;
 
