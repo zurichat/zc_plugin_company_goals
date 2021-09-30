d1519e208b105e63f5386b3fb28e4a03a1d2f73f
Ariiie <oluwafunmilayodada2018@gmail.com>
2021-09-30 20:43:14 +0100

    updated goal-detail but not done

--- client/src/components/GoalDetailAccordion/GoalDetail.styled.js
+++ client/src/components/GoalDetailAccordion/GoalDetail.styled.js
@@ -6,11 +6,12 @@ export const Div = styled.div`
   align-items: center;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  height: 250px;
+  height: 300px;
 `;
 
 export const Text = styled.h4`
   margin: 0 10px;
+  margin-bottom: 5px;
   font-size: 1rem;
   font-weight: 400;
   color: ${(props) => (props.primary ? '#393939' : '#8D8D8D')};
--- client/src/components/GoalDetailAccordion/GoalDetailData.js
+++ client/src/components/GoalDetailAccordion/GoalDetailData.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 import data from './GoalData';
 
@@ -7,8 +9,25 @@ import { Div, Text } from './GoalDetail.styled';
 const GoalDetailData = ({ goalData }) => {
   return (
     <Div>
-      <Text primary> {goalData.goal_name} </Text>
-      <Text> org goals </Text>
+      <Text primary>Goal Name</Text>
+      <Text> Wireframe </Text>
+      <Text primary>Category</Text>
+      <Text>Product Design</Text>
+      <Text primary>TimeLine</Text>
+      <Text>Sept 20 - Oct 31</Text>
+      <Text primary>Priority</Text>
+      <Text>
+        <Box>
+          <StarBorderIcon />
+          <StarBorderIcon />
+          <StarBorderIcon />
+          <StarBorderIcon />
+          <StarBorderIcon />
+        </Box>
+      </Text>
+      <Text primary>Goal Progress</Text>
+      <Text>Radio buttons</Text>
+      <Text primary>Create a Wireframe for a goal plugin</Text>
     </Div>
   );
 };
