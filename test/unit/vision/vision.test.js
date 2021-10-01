ffeadd5568bd1a0b1683351c418b53bdd78d085c
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-01 12:03:06 +0100

    test(vision): setup unit tests for GET vision route

--- test/unit/vision/vision.test.js
+++ test/unit/vision/vision.test.js
@@ -1,26 +1,35 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-expressions */
+const { default: axios } = require('axios');
 const { expect } = require('chai');
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const app = require('../../../app');
-const db = require('../../../db/databaseHelper');
+const logger = require('../../../utils/logger');
+const { orgID, visionRead } = require('../../globals');
 const sampleVision = require('./sampleVision.json');
+
+const URL = `/api/v1/vision/${orgID}`;
 
 chai.use(chaiHTTP);
 chai.use(sinonChai);
 
 const sandbox = sinon.createSandbox();
 
-describe('VISION', () => {
-  let findStub;
-  let sampleArgs;
+describe('VISION TESTS', () => {
+  let loggerStub;
+  let axiosStub;
 
   beforeEach(() => {
-    findStub = sandbox.stub(db, 'find').resolves(sampleVision);
+    // Disable logger messages
+    loggerStub = sandbox.stub(logger, 'info').returns('');
+    axiosStub = sandbox.stub(axios, 'get').returns({ data: { data: sampleVision } });
   });
 
-  // Refresh stubs and spies for each test
+  // Refresh sandbox for each test
   afterEach(() => {
     sandbox.restore();
   });
@@ -32,6 +41,24 @@ describe('VISION', () => {
         .get('/api/v1/vision')
         .end((err, res) => {
           expect(res).to.have.header('content-type', 'text/html; charset=UTF-8');
+          done();
+        });
+    });
+
+    it('Should get vision successfully', (done) => {
+      chai
+        .request(app)
+        .get(URL)
+        .end((err, res) => {
+          expect(err).to.not.be.an('error');
+          expect(res).to.have.status(200);
+          expect(loggerStub).to.have.been.called;
+          expect(axiosStub).to.have.been.calledOnce;
+          expect(axiosStub).to.have.been.calledWith(visionRead);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.property('status', 200);
+          expect(res.body).to.have.property('message', 'success');
+          expect(res).to.have.header('content-type', 'application/json; charset=utf-8');
           done();
         });
     });
