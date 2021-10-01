d4e876e6fc7bab894ba7a967ae218c8c6f3634f9
abiona01 <quadratwemimo@gmail.com>
2021-10-01 03:07:01 +0100

    feat: header; workspace user total and thumbnail

--- client/src/components/app_header/AppHeader.jsx
+++ client/src/components/app_header/AppHeader.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useMemo } from 'react';
-import { useParams } from 'react-router';
+import { useEffect, useState, useMemo } from 'react';
 import Parcel from 'single-spa-react/parcel';
 import { pluginHeader } from '@zuri/plugin-header';
 import { HeaderAppBar, HeaderToolBar, HeaderToolBarTitle } from './AppHeader.styled';
@@ -7,30 +6,50 @@ import HeaderAvatarGroup from './components/header_avatar_group/HeaderAvatarGrou
 import { GetWorkspaceUsers } from '@zuri/control';
 
 const AppHeader = () => {
-  let { orgId } = useParams();
-
+  const [total, settotal] = useState(null);
+  const GetUser = async () => {
+    try {
+      const info = await GetWorkspaceUsers();
+      console.log('bleh', info[99]);
+      console.log('bleh', info.totalUsers);
+      const whatever = {
+        thumbnailUrl: [info[0].image_url, info[1].image_url, info[2].image_url],
+        userCount: info.totalUsers,
+      };
+      settotal(whatever);
+    } catch (error) {
+      console.log('blehhhhhhh', error);
+      const whatever = {
+        thumbnailUrl: [
+          'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
+          'https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg',
+          'https://www.kemhospitalpune.org/wp-content/uploads/2020/12/Profile_avatar_placeholder_large.png',
+        ],
+        userCount: 50,
+      };
+      settotal(whatever);
+    }
+  };
   useEffect(() => {
-    const info = GetWorkspaceUsers();
-    console.log(info);
+    GetUser();
   }, []);
 
-  const headerConfig = useMemo(() => ({
-    name: 'Goals Plugin', //Name on header
-    icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4E8rvqCqoAlL7GUOvLTZn5amfp2pDTo5oNnBFnxhhFRCAqGVPCqGb2Yjb_Z9MK13BnXM&usqp=CAU', //Image on header
-    thumbnailUrl: [
-      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80',
-      'https://upload.wikimedia.org/wikipedia/en/7/70/Shawn_Tok_Profile.jpg',
-      'https://www.kemhospitalpune.org/wp-content/uploads/2020/12/Profile_avatar_placeholder_large.png',
-    ], //Replace with images of users
-    userCount: 50, //User count on header
-    eventTitle: () => {
-      //Block of code to be triggered on title click
-    },
-    eventThumbnail: () => {
-      //Block of code to be triggered on thumbnail click
-    },
-    hasThumbnail: true, //set false if you don't want thumbnail on the header
-  }));
+  const headerConfig = useMemo(
+    () => ({
+      name: 'Goals Plugin', //Name on header
+      icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4E8rvqCqoAlL7GUOvLTZn5amfp2pDTo5oNnBFnxhhFRCAqGVPCqGb2Yjb_Z9MK13BnXM&usqp=CAU', //Image on header
+      thumbnailUrl: total && total.thumbnailUrl, //Replace with images of users
+      userCount: total && total.userCount, //User count on header
+      eventTitle: () => {
+        //Block of code to be triggered on title click
+      },
+      eventThumbnail: () => {
+        //Block of code to be triggered on thumbnail click
+      },
+      hasThumbnail: true, //set false if you don't want thumbnail on the header
+    }),
+    [total]
+  );
   return pluginHeader ? (
     <Parcel config={pluginHeader} wrapWith="div" wrapStyle={{ width: '100%' }} headerConfig={headerConfig} />
   ) : (
