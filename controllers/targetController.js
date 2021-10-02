c5473607b240e48dd10c104d1856a5f8b08ffbcb
Destiny Aigbe <75917333+Destiny-01@users.noreply.github.com>
2021-10-02 11:12:11 +0100

    Fixed an async errror

--- controllers/targetController.js
+++ controllers/targetController.js
@@ -208,7 +208,7 @@ exports.getSingleGoalProgress = catchAsync(async (req, res, next) => {
     const result = calculate(goals, targets);
     const reduceResult = reduceCalculation(result);
     const keys = Object.keys(reduceResult);
-    keys.forEach((key) => {
+    keys.forEach(async(key) => {
       if (key === goal_id) {
         const finalResult = reduceResult[goal_id];
         await updateOne('goals', {progress: finalResult}, {}, org_id, goal_id
