7b1dbbec159f0845ed86327a66e3f5977624ba15
Isah Abba Ibrahim <38298865+Ibrahim-Isah@users.noreply.github.com>
2021-10-02 14:30:22 +0100

    Update item goal

--- client/src/components/Goals/GoalItem.jsx
+++ client/src/components/Goals/GoalItem.jsx
@@ -39,7 +39,7 @@ const GoalItem = ({ goalData }) => {
     .then(response => setTotalLikes(response.data.data.count))
     .catch(error => console.log(error))
 
-  }, [])
+  })
 
   const handleSetLike = (e) => {
     
@@ -64,7 +64,7 @@ const GoalItem = ({ goalData }) => {
     .then(response => setTotalDislikes(response.data.data.count))
     .catch(error => console.log(error))
 
-  }, [])
+  })
 
   const handleSetDislike = (e) => {
     e.stopPropagation();
