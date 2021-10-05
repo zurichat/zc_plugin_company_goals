8b83dff90dbab82044db1636a2823c557b76556c
Ariiie <oluwafunmilayodada2018@gmail.com>
2021-10-05 17:06:19 +0100

    added functionality to the goal detail-features to make some part of the goals dynamic

--- client/src/components/GoalDetailAccordion/GoalDetailData.js
+++ client/src/components/GoalDetailAccordion/GoalDetailData.js
@@ -1,32 +1,69 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Box } from '@material-ui/core';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
 import { openModal } from '../../redux/TargetModalSlice';
 import data from './GoalData';
 
 import { Div, Text, Button } from './GoalDetail.styled';
-import { makeStyles } from '@mui/material';
+import { makeStyles } from '@material-ui/core';
 
 const GoalDetailData = ({ goalData }) => {
   const dispatch = useDispatch();
+  const month = {
+    month_names: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ],
+    month_names_short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  };
+
+  // const Progress = Math.floor(goalData.progress ? goalData.progress : 0);
+  const goalStart = new Date(goalData.start_date);
+  const goalEnd = new Date(goalData.due_date);
+  const startMonth = month.month_names_short[goalStart.getMonth()];
+  const startDate = goalStart.getDate();
+  const endMonth = month.month_names_short[goalEnd.getMonth()];
+  const endDate = goalEnd.getDate();
+
+  const useStyles = makeStyles({
+    icon: {
+      color: '#bdbdbd',
+    },
+  });
+
+  const classes = useStyles();
 
   return (
     <React.Fragment>
       <Div>
         <Text primary> Goal Name</Text>
-        <Text> Wireframe </Text>
+        <Text>{goalData.goal_name ? goalData.goal_name : 'No name'} </Text>
         <Text primary> Category</Text>
-        <Text>Product Design</Text>
+        <Text>Product design</Text>
         <Text primary>Timeline </Text>
-        <Text>Sept 20 - Oct 31</Text>
+        <Text>
+          {goalData.start_date && goalData.due_date
+            ? `${startMonth} ${startDate} - ${endMonth} ${endDate}`
+            : 'No date set'}
+        </Text>
         <Text primary>Priority</Text>
         <Text>
           <Box>
-            <StarBorderIcon />
-            <StarBorderIcon />
-            <StarBorderIcon />
-            <StarBorderIcon />
+            <StarIcon className={classes.icon} />
+            <StarIcon className={classes.icon} />
+            <StarIcon className={classes.icon} />
+            <StarIcon className={classes.icon} />
           </Box>
         </Text>
         <Text primary>Goal Progress</Text>
@@ -81,7 +118,7 @@ const GoalDetailData = ({ goalData }) => {
         <Text primary>Goal Description</Text>
         <Text>Create a Wireframe for a goal plug In</Text>
 
-        {/* <Text primary> {goalData.goal_name} </Text>
+        {/* <Text primary>{goalData.goal_name} </Text>
         <Text> org goals </Text> */}
 
         {/* <Button
