8f438b89d6afa01941f3af6863898cdf64f2d9d7
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-14 22:12:49 +0100

    feat(suggestions): implement search-suggestions endpoint

---
+++ controllers/suggestionController.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line no-unused-vars
+const { request, response } = require('express');
+const { getSuggestions } = require('../services/suggestions.service');
+
+/**
+ * Search controller
+ * @param {request} req Express request object
+ * @param {response} res Express response object
+ */
+exports.searchSuggestions = async (req, res) => {
+  const { org_id: orgID, member_id: memberID } = req.params;
+
+  try {
+    const result = await getSuggestions(orgID, memberID);
+
+    if (result instanceof Error) {
+      throw result;
+    }
+
+    const payload = {
+      status: 'ok',
+      statusCode: 200,
+      type: 'suggestions',
+      data: result,
+    };
+
+    return res.status(200).json(payload);
+  } catch (error) {
+    if (error.isOperational) {
+      return res
+        .status(error.statusCode)
+        .json({ message: error.message, error: error.status, statusCode: error.statusCode });
+    }
+    return res.status(500).json({ status: 'fail', statusCode: 500, message: error.message });
+  }
+};
