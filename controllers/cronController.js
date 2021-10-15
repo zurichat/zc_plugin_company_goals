00aa9f75da010bf2d8329df3684d02fa6e639547
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-10-15 13:49:16 +0100

    feat: write to console to test centrifugo

--- controllers/cronController.js
+++ controllers/cronController.js
@@ -42,6 +42,7 @@ module.exports = () => {
   });
 
   cron.schedule('*/10 * * * * *', async () => {
+    console.log('i publish');
     await publish('centrifugo-is-working', { data: 'Centrifugo is now working' });
   });
 };
